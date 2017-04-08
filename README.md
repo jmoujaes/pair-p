4e69fd34ecf5f9b9aec573c5cedab1dbc1985ed8
Jason Moujaes <jasonmou@yahoo.com>
2017-04-08 12:09:14 -0700

    Update README with setup information. Create build script to set up virtual environment and install required python packages. Add Flask and eventlet, and their dependencies as requirements. Pull in the source code of google's diff_match_patch that we'll use to update the file that is being collaborated on.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pair-p
+
+Prerequisites: python3
+
+To set-up your development environment, run the build script -- ./build.sh
---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# trap errors and print failure info
+trap ': "*** BUILD FAILED ***" $BASH_SOURCE:$LINENO: error: "$BASH_COMMAND" returned $?' ERR
+
+# print commands executed, exit on errors
+set -eExuo pipefail
+
+# print version of bash we're using
+echo "BASH VERSION: $BASH_VERSION"
+
+# set the expected pip version
+PIP_VERSION=9.0.1
+
+rm -rf ./build/
+mkdir -p build
+
+# create virtual environment
+virtualenv -p `which python3` ./build/.env
+
+cd ./build/.env
+set +u
+source ./bin/activate
+set -u
+
+# upgrade pip
+pip install --upgrade pip==${PIP_VERSION} 
+
+# emit python and pip versions
+python --version
+pip --version
+
+# install python dependencies
+pip install -r ../../requirements.txt --no-cache-dir
+
+cd ../../
+
+# basic syntax checking
+python -m compileall *.py
+
+# TODO pylint
+# TODO unittests
+
+# success
+: "*** BUILD SUCCESSFUL ***"
+
